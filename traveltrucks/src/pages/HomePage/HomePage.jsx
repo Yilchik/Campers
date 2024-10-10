6c4c2749298b0874b8ccde4a6d97eac2cb35ee87
Yilchik <yulyayulia2@gmail.com>
2024-10-10 22:46:31 +0200

    homePage css

--- traveltrucks/src/pages/HomePage/HomePage.jsx
+++ traveltrucks/src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,15 @@ const HomePage = () => {
     <div>
       <Header />
       <div className={css.container}>
-        <h1>Campers of your dreams</h1>
-        <p>You can find everything you want in our catalog</p>
+        <div className={css.title}>
+          <h1>Campers of your dreams</h1>
+          <p className={css.text}>
+            You can find everything you want in our catalog
+          </p>
+        </div>
+        <button type="button" className={css.btn}>
+          View Now
+        </button>
       </div>
     </div>
   );
